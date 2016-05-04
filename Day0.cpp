9424019bb4100ae62c0580da2bc4b9efe08e23b4
leonore13 <shinjitsu13@gmail.com>
2016-05-03 19:28:39 -0700

    Added files via upload

---
+++ Day0.cpp
@@ -0,0 +1,21 @@
+//
+//  Day0.cpp
+//  HackerRank 30DaysOfCode
+//
+//  Created by Sophia Nguyen on 2016-05-03.
+//  Copyright © 2016 Sophia Nguyen. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string inputString; // declare a variable to hold our input
+    getline(cin, inputString); // get a line of input from cin and save into variable
+    
+    cout << "Hello, World." << endl;
+    cout << inputString << endl;
+    
+    return 0;
+}
